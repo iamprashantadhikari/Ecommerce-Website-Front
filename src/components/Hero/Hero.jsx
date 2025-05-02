10a5d0de35c4b7ae0d970bfa44562f29c238cd1c
prashantadhikari1 <prashantadk13@gmail.com>
2025-05-02 08:18:29 +0545

    Products and Animation added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -39,31 +39,33 @@ const Hero = () => {
     pauseOnFocus: false
   }
 
-  const [data, setData] = useState(null)
-  const [error, setError] = useState(null)
-  useEffect(()=>{
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://admin.maitigaun.com/api/mobile/v1/ecommerce/sliders')
-        if(!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const result = await response.json()
-        setData(result);
-        // console.log(result)
-      } catch (error) {
-        setError(error.message)
-      }
-    }
-    fetchData();
-  }, []);
+  // For Api Code Starts Here
+  // const [data, setData] = useState(null)
+  // const [error, setError] = useState(null)
+  // useEffect(()=>{
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch('https://admin.maitigaun.com/api/mobile/v1/ecommerce/sliders')
+  //       if(!response.ok) {
+  //         throw new Error('Network response was not ok');
+  //       }
+  //       const result = await response.json()
+  //       setData(result);
+  //       // console.log(result)
+  //     } catch (error) {
+  //       setError(error.message)
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
 
-  if (error) {
-    return <div className="text-red-500">Error: {error}</div>;
-  }
-  if (!data) {
-    return <div className="text-center py-8">Loading sliders...</div>;
-  }
+  // if (error) {
+  //   return <div className="text-red-500">Error: {error}</div>;
+  // }
+  // if (!data) {
+  //   return <div className="text-center py-8">Loading sliders...</div>;
+  // }
+  // For Api Code Ends Here
 
   return (
     <div className='relative overflow-hidden min-h-[550px] sm:min-h-[650px]
@@ -77,7 +79,7 @@ const Hero = () => {
         {/* Hero Section */}
         <div className="container pb-8 sm:pb-0">
           <Slider {...settings}>
-            {data.data?.map((item) => (
+            {ImageList?.map((item) => (
           <div key={item.id}>
             <div className="grid grid-cols-1 sm:grid-cols-2">
               {/* text content section */}
@@ -85,13 +87,24 @@ const Hero = () => {
               className='flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center
               sm:text-left order-2 sm:order-1 relative z-10'
               >
-                <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>
+                <h1
+                data-aos="zoom-out"
+                data-aos-duration="500"
+                data-aos-once="true"
+                className='text-5xl sm:text-6xl lg:text-7xl font-bold'>
                   {item.title}</h1>
-                <p className='text-sm'>
-                  {item.name}
+                <p
+                data-aos="fade-up"
+                data-aos-duration="500"
+                data-aos-delay="100"
+                className='text-sm'>
+                  {item.description}
                 </p>
                 <div>
                   <button
+                  data-aos="fade-up"
+                  data-aos-duration="500"
+                  data-aos-delay="300"
                   className='bg-gradient-to-r from-primary to-secondary hover:scale-105
                   duration-200 text-white py-2 px-4 rounded-full'>
                     Order Now
@@ -101,7 +114,7 @@ const Hero = () => {
               {/* Image Section */}
               <div className='order-1 sm:order-2'>
                 <div className='relative z-10'>
-                  <img src={item.image} alt="" 
+                  <img src={item.img} alt="" 
                   className='w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125
                   lg:scale-120 object-contain mx-auto'
                   />
