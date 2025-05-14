174c5d4ba504dfc5ad95124d5179b418e1ec657c
prashantadhikari1 <prashantadk13@gmail.com>
2025-05-14 11:54:58 +0545

    Bug Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
-import Products from './components/Products/products'
+import Products from './components/Products/Products'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 import TopProducts from './components/TopProducts/TopProducts'
